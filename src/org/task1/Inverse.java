9fbe33f6cc56fec98ad569329e0450f40bf01458
Aruchamy25 <aruchamy25@gmail.com>
2022-03-14 13:07:44 +0530

    done line is add

--- src/org/task1/Inverse.java
+++ src/org/task1/Inverse.java
@@ -4,12 +4,13 @@ public class Inverse {
 	public static void main(String[] args) {
 		int a=123457; int top=0; int rev=0; int rem=0;
 		while (a%10!=0) {
+			
 			rem=a%10;
 			rev= rev*10+rem;
 			a=a/10;
 			
 		}
 		System.out.println(rev);
+		System.out.println("work is done");
 	}
-
 }
