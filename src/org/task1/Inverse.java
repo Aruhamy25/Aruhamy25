1ade02bbd720fb9f1d4fcb8927ba935c219cfc4e
Aruchamy25 <aruchamy25@gmail.com>
2022-03-14 12:56:09 +0530

    new class file added

---
+++ src/org/task1/Inverse.java
@@ -0,0 +1,15 @@
+package org.task1;
+
+public class Inverse {
+	public static void main(String[] args) {
+		int a=123457; int top=0; int rev=0; int rem=0;
+		while (a%10!=0) {
+			rem=a%10;
+			rev= rev*10+rem;
+			a=a/10;
+			
+		}
+		System.out.println(rev);
+	}
+
+}
