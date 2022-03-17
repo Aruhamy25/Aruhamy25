40168974ff0e5d9a8b0535a2c12b74f83322031b
Aruchamy25 <aruchamy25@gmail.com>
2022-03-17 10:20:47 +0530

    aaaa

--- src/org/task1/Inverse.java
+++ src/org/task1/Inverse.java
@@ -14,6 +14,7 @@ public class Inverse {
 		System.out.println("work is done");
 		System.out.println("sakthi....");
 		System.out.println("Raj2....");
+		System.out.println("Aru..............................");
 
 	}
 }
